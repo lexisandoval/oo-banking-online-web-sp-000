b38bfcd0acdcf41b320950387d08cd47c7068a86
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-02 22:24:15 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,12 @@
 class BankAccount
 
-  attr_accessor :name, :balance
+  attr_accessor :name, :balance, :status
 
   def initialize(name)
 
     @name = name
     @balance = 100
+    @status = "open"
 
   end
 
