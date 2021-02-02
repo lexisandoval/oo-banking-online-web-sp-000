1441062117d6d4447986a855c0e2fc88281ce2b5
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-02 22:26:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,11 +4,13 @@ class BankAccount
   attr_reader :name
 
   def initialize(name)
-
     @name = name
     @balance = 1000
     @status = "open"
+  end
 
+  def deposit(money)
+    @balance << money
   end
 
 end
